2a8cc2e069db3f8bb677850d5d9cecf724bde655
IvanBurk <64144110+IvanBurk@users.noreply.github.com>
2021-08-30 17:47:46 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statisticsTest/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statisticsTest/StatisticsServiceTest.java
@@ -1,8 +1,6 @@
 package ru.netology.statisticsTest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.statistics.StatisticsService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
